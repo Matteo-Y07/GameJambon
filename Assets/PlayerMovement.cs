ad2d8a98409eb967dc35d281834f2a4274e238c8
Maelan <maelan.peyron@etu.univ-amu.fr>
2026-04-08 11:03:01 +0200

    blob

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,16 +11,15 @@ public class PlayerMovement : MonoBehaviour
     public Transform GroundCheckLeft;
     public Transform GroundCheckRight;
     public LayerMask groundLayer;
-
     public bool isGrounded= false;
     public bool hasJump = true;
 
     // Dash variables
-    public float dashPower = 10f;
+    public float dashPower = 25f;
     public Vector2 dashDirection = new Vector2();
     public bool isDashing = false;
     public bool hasDash = true;
-    
+    public float gravity = 2.5f;
 
     public SpriteRenderer PlayerSpriteRenderer;
 
@@ -28,7 +27,9 @@ public class PlayerMovement : MonoBehaviour
     {
         // prends le SpriteRenderer du joueur pour pouvoir le tourner à gauche ou à droite
         PlayerSpriteRenderer = GetComponent<SpriteRenderer>();
+        
     }
+    
    
     void Update()
     {
@@ -44,17 +45,17 @@ public class PlayerMovement : MonoBehaviour
     void KeyboardInput()
     {
         // Déplacement horizontal
-        if (Input.GetKey("Horizontal") && !isDashing) playerMoveRight();
+        if (Input.GetAxisRaw("Horizontal") > 0 && !isDashing) playerMoveRight();
 
-        else if (Input.GetKey("Horizontal") && !isDashing) playerMoveLeft();
+        else if (Input.GetAxisRaw("Horizontal") < 0 && !isDashing) playerMoveLeft();
 
         else if (!isDashing) playerStop();
 
         // Dash
-        if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && !hasJump && hasDash) StartCoroutine(playerDash());
+        if (Input.GetButtonDown("Jump") && !isGrounded && !hasJump && hasDash) StartCoroutine(playerDash());
 
         // Jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+        if (Input.GetButtonDown("Jump") && isGrounded) 
         {
             hasJump = true;
             playerJump();
@@ -87,20 +88,22 @@ public class PlayerMovement : MonoBehaviour
         Vector2 dashDirection = Vector2.zero;
 
         // dash dans la direction du mouvement
-        if (Input.GetKeyDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0) dashDirection.x += 1f;   // droite
-        if (Input.GetKeyDown("Horizontal") && Input.GetAxisRaw("Horizontal") < 0) dashDirection.x -= 1f;   // gauche
-        if (Input.GetKeyDown("Vertical") && Input.GetAxisRaw("Vertical") > 0) dashDirection.y += 1f;   // haut
-        if (Input.GetKeyDown("Vertical") && Input.GetAxisRaw("Vertical") < 0) dashDirection.y -= 1f;   // bas
+        if (Input.GetAxisRaw("Horizontal") > 0) dashDirection.x += 1f;   // droite
+        if (Input.GetAxisRaw("Horizontal") < 0) dashDirection.x -= 1f;   // gauche
+        if (Input.GetAxisRaw("Vertical") > 0) dashDirection.y += 1f;   // haut
+        if (Input.GetAxisRaw("Vertical") < 0) dashDirection.y -= 1f;   // bas
 
         // Si aucune touche, dash vers le haut
         if (dashDirection == Vector2.zero)
             dashDirection = Vector2.up;
 
         dashDirection.Normalize(); // On normalise la direction pour que les diagonales ne soient pas plus rapides (pas comme dans Minecraft)
+        rb.gravityScale = 0f; // On désactive la gravité pendant le dash
         rb.velocity = dashDirection * dashPower; // On dash
 
         yield return new WaitForSeconds(0.1f); // Durée du dash
         rb.velocity = dashDirection * moveSpeed; // On remet la vitesse qu'on avait avant le dash
+        rb.gravityScale = gravity; // On réactive la gravité
         isDashing = false;
     }
 
@@ -108,7 +111,7 @@ public class PlayerMovement : MonoBehaviour
     {
         isGrounded = Physics2D.OverlapArea(GroundCheckLeft.position, GroundCheckRight.position, groundLayer);
         if (isGrounded){
-            hasJump = true;
+            hasJump = false;
             hasDash = true; // Récupère le dash quand on touche le sol
         }
     }
