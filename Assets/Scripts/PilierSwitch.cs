f118495a38bb3ae826d3b8b95f0c838fd54f8a19
MaelanP77 <maelan.peyron@etu.univ-amu.fr>
2026-04-23 14:41:43 +0200

    ajout du sanctuaire pour purifier la zone et du script

--- Assets/Scripts/PilierSwitch.cs
+++ Assets/Scripts/PilierSwitch.cs
@@ -2,40 +2,68 @@ using UnityEngine;
 
 public class PillarSwitch : MonoBehaviour
 {
+    [Header("Sprites")]
+    [SerializeField] private GameObject normalGO;
+    [SerializeField] private GameObject highlightGO;
+
     [Header("Tilemaps")]
     [SerializeField] private GameObject tilemapDead;
     [SerializeField] private GameObject tilemapAlive;
 
     private bool playerInside = false;
+    private float switchCooldown = 2f;
+    private float cooldownTimer = 0f;
+    void Start()
+    {
+        normalGO.SetActive(true);
+        highlightGO.SetActive(false);
+
+        tilemapDead.SetActive(true);
+        tilemapAlive.SetActive(false);
+    }
 
     void Update()
     {
         if (!playerInside) return;
 
+        // Appuie sur S
         if (Input.GetAxisRaw("Vertical") < 0)
         {
-            ChangeTilemap();
+            cooldownTimer += Time.deltaTime;
+            if (cooldownTimer < switchCooldown) return;
+            else
+            {
+                SwitchTilemaps();
+                cooldownTimer = 0f;
+            }
         }
     }
 
-    void ChangeTilemap()
+    void SwitchTilemaps()
     {
-        if (tilemapDead != null)
-            tilemapDead.SetActive(false);
-
-        if (tilemapAlive != null)
-            tilemapAlive.SetActive(true);
+        tilemapDead.SetActive(false);
+        tilemapAlive.SetActive(true);
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
+        {
             playerInside = true;
+
+            normalGO.SetActive(false);
+            highlightGO.SetActive(true);
+        }
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
+        {
             playerInside = false;
+
+            normalGO.SetActive(true);
+            highlightGO.SetActive(false);
+        }
     }
 }
