6f1b4e13e2d0f7d0a87716bd49b48dba0aac9604
MaelanP77 <maelan.peyron@etu.univ-amu.fr>
2026-05-27 18:27:31 +0200

    ajout du monstre sleep fonctionnel reste monstre normal bug vie, monstre pollution déplacement, monstre debuff accumule les debuff

--- Assets/Scripts/Monster/MonsterSleep.cs
+++ Assets/Scripts/Monster/MonsterSleep.cs
@@ -1,31 +1,68 @@
 using UnityEngine;
-using System.Collections;
 
 public class MonsterSleep : Monster
 {
     [Header("Sleep")]
     [SerializeField] private float sleepDuration = 2f;
 
+    private PlayerMovement playerMovement;
+
+    private bool isSleeping = false;
+    private float timer = 0f;
+
+    private Collider2D monsterCollider;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        monsterCollider = GetComponent<Collider2D>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (isSleeping)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0f)
+            {
+                playerMovement.enabled = true;
+
+                Destroy(gameObject);
+            }
+        }
+    }
+
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
         base.OnCollisionEnter2D(collision);
-        if (!collision.gameObject.CompareTag("Player")) return;
-        PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
 
-        if (player != null)
+        if (!collision.gameObject.CompareTag("Player"))
+            return;
+
+        if (isSleeping)
+            return;
+
+        playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
+
+        if (playerMovement != null)
         {
-            StartCoroutine(SleepPlayer(player));
+            playerMovement.enabled = false;
+
+            timer = sleepDuration;
+            isSleeping = true;
+
+            // rend le monstre invisible
+            spriteRenderer.enabled = false;
+
+            // désactive les collisions
+            monsterCollider.enabled = false;
+
+            // stop le mouvement
+            rb.velocity = Vector2.zero;
+            rb.simulated = false;
         }
-        GameObject.Destroy(gameObject);
-    }
-
-    IEnumerator SleepPlayer(PlayerMovement player)
-    {
-        player.enabled = false;
-        Debug.Log("Player asleep for " + sleepDuration + " seconds");
-        yield return new WaitForSeconds(sleepDuration);
-        player.enabled = true;
-        Debug.Log("Player awake");
-
     }
 }
